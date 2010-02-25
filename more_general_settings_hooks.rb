b712d03325cd7b321fc7e96831bfbef317f5a7ee
Steph Powell <steph@endpoint.com>
2010-02-25 15:45:29 -0700

    Added categorization of settings. Added stylesheet to general settings via admin_hook.

--- more_general_settings_hooks.rb
+++ more_general_settings_hooks.rb
@@ -1,4 +1,5 @@
 class MoreGeneralSettingsHooks < Spree::ThemeSupport::HookListener
   insert_before :show_general_settings, 'admin/general_settings/show_more'
   insert_before :edit_general_settings, 'admin/general_settings/edit_more'
+  insert_after :admin_inside_head, 'admin/general_settings/styles'
 end
